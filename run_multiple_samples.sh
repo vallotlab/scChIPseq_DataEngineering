0aaeb28b1a6d12b02ba0e35eec492397b84bfe5d
Gregjlt <gregoire.jouault@curie.fr>
2023-05-09 10:56:46 +0200

    Change species_config_file in run_multiple_samples

--- run_multiple_samples.sh
+++ run_multiple_samples.sh
@@ -54,7 +54,7 @@ do
 
   if [[ $MARK == "h3k27me3" && $ASSEMBLY == "hg38" ]]
   then
-  OUTPUT_CONFIG=/data/tmp/gjouault/results/CONFIG_HUMAN_scCutTag_InDrop_K27
+  OUTPUT_CONFIG=/data/tmp/gjouault/results/CONFIG_HUMAN_scCutTag_InDrop_K27_${ASSEMBLY}_${FINAL_NAME}
   fi
   if [[ $MARK == "h3k4me3" && $ASSEMBLY == "hg38" ]]
   then
@@ -67,21 +67,25 @@ do
   DESIGN_TYPE=LBC
   if [[ $MARK == "h3k27me3" && $ASSEMBLY == "mm10" ]]
   then
-  OUTPUT_CONFIG=/data/tmp/gjouault/results/CONFIG_MOUSE_scCutTag_InDrop_H3K27ME3
+  OUTPUT_CONFIG=/data/tmp/gjouault/results/CONFIG_MOUSE_scCutTag_InDrop_H3K27ME3_${ASSEMBLY}_${FINAL_NAME}
   fi
   if [[ $MARK == "h3k4me3" && $ASSEMBLY == "mm10" ]]
   then
   OUTPUT_CONFIG=/data/tmp/gjouault/results/CONFIG_MOUSE_scCutTag_InDrop_H3K4ME3
   fi
 
-  ./schip_processing.sh GetConf --template  CONFIG_TEMPLATE --configFile species_design_configs.csv --designType ${DESIGN_TYPE} --genomeAssembly ${ASSEMBLY} --outputConfig ${OUTPUT_CONFIG} --mark ${MARK}
+  ./schip_processing.sh GetConf --template  CONFIG_TEMPLATE --configFile species_design_configs_3.csv --designType ${DESIGN_TYPE} --genomeAssembly ${ASSEMBLY} --outputConfig ${OUTPUT_CONFIG} --mark ${MARK}
  
   OUTPUT_DIR=/data/kdi_prod/project_result/1184/02.00/results/scCutTag/${ASSEMBLY}/${FINAL_NAME}
-  READ1=/data/kdi_prod/dataset/${DATASET_NUMBER}/export/user/${DATASET_NAME}/${DATASET_NAME}.R1.fastq.gz
-  READ2=/data/kdi_prod/dataset/${DATASET_NUMBER}/export/user/${DATASET_NAME}/${DATASET_NAME}.R2.fastq.gz
-  READ3=/data/kdi_prod/dataset/${DATASET_NUMBER}/export/user/${DATASET_NAME}/${DATASET_NAME}.R3.fastq.gz
+#  READ1=/data/kdi_prod/dataset/${DATASET_NUMBER}/export/user/${DATASET_NAME}/${DATASET_NAME}.R1.fastq.gz
+#  READ2=/data/kdi_prod/dataset/${DATASET_NUMBER}/export/user/${DATASET_NAME}/${DATASET_NAME}.R2.fastq.gz
+#  READ3=/data/kdi_prod/dataset/${DATASET_NUMBER}/export/user/${DATASET_NAME}/${DATASET_NAME}.R3.fastq.gz
 
-echo "cd /data/users/gjouault/GitLab/scCutTag_InDrop; ./schip_processing.sh All --forward ${READ_1} --reverse ${READ_3} --index ${READ_2} -c ${OUTPUT_CONFIG}  -o ${OUTPUT_DIR} --name ${FINAL_NAME}" # | qsub -l "nodes=1:ppn=8,mem=60gb" -N job_${FINAL_NAME}_${ASSEMBLY}
+  READ_1=${BCL_DIR}/${NGS_NAME}.R1.fastq.gz
+  READ_2=${BCL_DIR}/${NGS_NAME}.R2.fastq.gz
+  READ_3=${BCL_DIR}/${NGS_NAME}.R3.fastq.gz
+
+echo "cd /data/users/gjouault/GitLab/scCutTag_InDrop;./schip_processing.sh All --forward ${READ_1} --reverse ${READ_3} --index ${READ_2} --conf ${OUTPUT_CONFIG} --output ${OUTPUT_DIR} --name ${FINAL_NAME}"| qsub -l nodes=1:ppn=10,mem=60gb -N job_${FINAL_NAME}_${ASSEMBLY}
 
 
 done < "$sample_sheet"
